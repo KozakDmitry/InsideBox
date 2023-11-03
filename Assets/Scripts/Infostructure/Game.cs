2a6da528011a6ff1d09fbafaa77fcf55f2c2a9b5
Dmitry Kozak <dimakaza4ka@gmail.com>
2023-11-03 15:44:17 +0300

    Added select for input

--- Assets/Scripts/Infostructure/Game.cs
+++ Assets/Scripts/Infostructure/Game.cs
@@ -1,9 +1,28 @@
-﻿namespace Scripts.Infostructure
+﻿using Scripts.Services.Input;
+using UnityEngine;
+
+namespace Scripts.Infostructure
 {
     public class Game
     {
         public Game()
         {
+            RegisterInputMethod();
         }
+
+        private static void RegisterInputMethod()
+        {
+            if (Application.isEditor)
+            {
+                inputService = new StandaloneInputService();
+            }
+            else
+            {
+                inputService = new MobileInputService();
+            }
+        }
+
+        public static IInputService inputService;
+    
     }
 }
--- Assets/Scripts/Services/Input/InputService.cs
+++ Assets/Scripts/Services/Input/InputService.cs
@@ -14,29 +14,9 @@ namespace Scripts.Services.Input
 
         public bool IsAttackButtonUp() =>
             SimpleInput.GetButtonUp(Button);
-    }
 
-    class StandaloneInputService : InputService
-    {
-        public override Vector2 Axis
-        {
-            get
-            {
-                Vector2 axis = SimpleInputAxis();
 
-                if (axis == Vector2.zero)
-                {
-                    axis = UnityAxis();
-                }
-                return axis;
-
-            }
-        }
-
-        private static Vector2 SimpleInputAxis() => 
+        protected static Vector2 SimpleInputAxis() => 
             new(SimpleInput.GetAxis(Horizontal), SimpleInput.GetAxis(Vertical));
-
-        private static Vector2 UnityAxis() => 
-            new(UnityEngine.Input.GetAxis(Horizontal), UnityEngine.Input.GetAxis(Vertical));
     }
 }
