ba5e2ff9461315dd30066d06517eefdb5d703246
Dmitry Kozak <dimakaza4ka@gmail.com>
2023-11-12 10:59:49 +0300

    Changed code a bit

---
+++ Assets/Scripts/Logic/SaveTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using Infostructure.Services.SaveLoad;
+using UnityEngine;
+
+namespace Scripts.Logic
+{
+    public class SaveTrigger : MonoBehaviour
+    {
+        private ISaveLoadService _saveLoadService;
+
+        public BoxCollider collider;
+
+        private void Awake()
+        {
+            _saveLoadService = AllServices.Container.Single<ISaveLoadService>();
+        }
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            _saveLoadService.SaveProgress();
+
+            gameObject.SetActive(false);
+        }
+
+
+        private void OnDrawGizmos()
+        {
+            if (!collider)
+            {
+                return;
+            }
+
+            Gizmos.color = new Color32(30, 200, 30, 130);
+            Gizmos.DrawCube(transform.position + collider.center, collider.size);
+        }
+    }
+}
